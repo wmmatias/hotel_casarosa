da8e6f303bbddf706e398df798c104270f4c226a
wmmatias <lard0595@gmail.com>
2022-11-25 08:18:24 +0800

    with sql

--- SQL/fmitr_casarosa_rooms.sql
+++ SQL/fmitr_casarosa_rooms.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `fmitr_casarosa` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `fmitr_casarosa`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: fmitr_casarosa
@@ -29,14 +27,25 @@ CREATE TABLE `rooms` (
   `room_number` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
   `room_type` int(11) DEFAULT NULL,
+  `room_rate` varchar(255) DEFAULT NULL,
   `user_id` int(11) NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`,`room_number`,`user_id`),
   KEY `fk_rooms_users_idx` (`user_id`),
   CONSTRAINT `fk_rooms_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rooms`
+--
+
+LOCK TABLES `rooms` WRITE;
+/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
+INSERT INTO `rooms` VALUES (1,101,'Junior First Level',1,'1000.00',1,'2022-11-09 02:36:40','2022-11-12 11:57:17'),(4,201,'Standard First Level',2,'1000.00',1,'2022-11-09 03:02:45','2022-11-09 03:02:45'),(5,301,'Intermediate First Level',3,'1800.00',1,'2022-11-09 03:03:56','2022-11-13 10:48:48'),(6,401,'Twin First Level',4,'1790.00',1,'2022-11-09 03:04:12','2022-11-13 12:45:42'),(7,501,'Family First Level',5,'1780.00',1,'2022-11-09 03:04:28','2022-11-09 03:04:28'),(8,102,'Junior First Level',1,'1500.00',1,'2022-11-09 07:36:19','2022-11-09 07:36:19'),(9,202,'Standard First Level',2,'1345.00',1,'2022-11-09 07:36:28','2022-11-09 07:36:28'),(10,302,'Intermediate First Level',3,'1800.00',1,'2022-11-09 07:36:38','2022-11-09 07:36:38'),(11,402,'Twin First Level',4,'1790.00',1,'2022-11-09 07:36:51','2022-11-09 07:36:51'),(12,502,'Family First Level',5,'1670.00',1,'2022-11-09 07:37:38','2022-11-09 07:37:38');
+/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -47,4 +56,4 @@ CREATE TABLE `rooms` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-12  2:24:29
+-- Dump completed on 2022-11-25  8:17:13
--- SQL/fmitr_casarosa_users.sql
+++ SQL/fmitr_casarosa_users.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `fmitr_casarosa` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `fmitr_casarosa`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: fmitr_casarosa
@@ -34,8 +32,18 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin','admin','admin@gmail.com','ca87f2beaffea8856dffacdbfb91da2e',0,'2022-11-09 03:45:18','2022-11-09 04:26:58'),(4,'WILARD','MATIAS','lard0595@gmail.com','ca87f2beaffea8856dffacdbfb91da2e',1,'2022-11-24 14:27:14','2022-11-25 00:02:52');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -46,4 +54,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-11-12  2:24:29
+-- Dump completed on 2022-11-25  8:17:13
